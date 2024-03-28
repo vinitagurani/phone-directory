5d35b36d9f81b0dbb5b5c111e6c0f74a65934e9e
vinita gurnani <vinitagurnani6@gmail.com>
2024-03-28 19:21:59 +0530

    Created skeleton of home page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
 function App() {
   return (
-    // <div className="App">
-    <div>
-      Phone directory
+    <div> {/*one parent element*/}
+      <div className="header">
+        {/* difference in classname */}
+        Phone directory
+      </div>
+      <button> Add </button>
+      <div>
+        <span>Name</span> <br /> {/*difference forward slash*/}
+        <span>Phone</span>
+      </div>
+      <label htmlFor = "name" >Name: </label>
+      <input id ="name" type="text" placeholder="Type here" defaultValue= "Vinita"></input>
     </div>
   );
 }
