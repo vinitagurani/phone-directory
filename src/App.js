28b0b6d8a377b6e2d6d6c30b26b9a5c4d748033c
vinita gurnani <vinitagurnani6@gmail.com>
2024-03-31 14:22:26 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import "./App.css"
 function App() {
   // let x = 1;
   // let y = 2;
+  let subscribers = [
+    {
+      id: 1,
+      name: 'Vinita',
+      phone: 888888888
+    },
+    {
+      id: 2,
+      name: 'Shilpa',
+      phone: 777777777
+    }
+  ]
   return (
     <div> {/*one parent element*/}
       <Header/>
@@ -15,6 +27,14 @@ function App() {
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+
+          {subscribers.map(sub => {
+            return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            
+          })}
       </div>
     </div>
   );
